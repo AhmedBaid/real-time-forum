61216ce824fe394096200f8d02728d84e7beae7f
AhmedBaid <baidahmed2005@gmail.com>
2025-07-25 16:15:56 +0100

    register page

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,8 +1,8 @@
-let container = document.querySelector(".main")
-function loginpage() {
-    let page = document.createElement("div")
+// let container = document.querySelector(".main")
+// function loginpage() {
+//     let page = document.createElement("div")
 //     page.innerHTML = `
 // `
-    container.appendChild(page)
-}
-loginpage()
+//     container.appendChild(page)
+// }
+// loginpage()
