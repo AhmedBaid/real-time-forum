c9ac3aff1659db3532030ed59d047dd1e05e1c72
AhmedBaid <baidahmed2005@gmail.com>
2025-07-27 18:19:38 +0100

    working on logout

--- frontend/src/js/pages/home.js
+++ frontend/src/js/pages/home.js
@@ -1,8 +1,24 @@
 import { container } from "../config.js";
 
 export function home(data) {
-    container.innerHTML = "";
     const username = data.username;
-    container.innerHTML = `Welcome ${username}!`;
+    let header = document.createElement("header")
+    header.className = "header";
+    container.innerHTML = "";
+    header.innerHTML = `<h1>Forum Welcome ${username}</h1>
+    <form method="post">
+        <button class="logout">Logout</button>
+    </form>`;
+    container.appendChild(header);
+    const logoutButton = header.querySelector(".logout");
+    logoutButton.addEventListener("click", Logout)
 }
-
+function Logout(e) {
+    e.preventDefault();
+    let response = fetch("/logout")
+    if (!response.ok) {
+        console.error("Logout failed");
+        return;
+    }
+    home("/login")
+}
