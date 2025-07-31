6a6cc25520bb1010b1e91f04b1ee4663d4368d53
Your Name <you@example.com>
2025-07-31 15:13:16 +0100

    show comments and fix style

--- frontend/src/js/views/home.js
+++ frontend/src/js/views/home.js
@@ -1,6 +1,6 @@
 import { container, Header, Navigate, PostForm } from "../config.js";
 import { login } from "./login.js";
-
+import { timeFormat } from "../helpers/timeFormat.js";
 async function fetchPosts() {
   try {
     let res = await fetch("/getPosts");
@@ -17,14 +17,17 @@ async function fetchPosts() {
 export async function home() {
   let header = document.createElement("header");
   let Postform = document.createElement("div");
-  // posts 
-  let allPost  = document.createElement("div")
+  // posts
+  let allPost = document.createElement("div");
+  allPost.className = "allPost";
   let obj = await fetchPosts();
+  console.log(obj.data.Posts[0].comments);
 
   for (const post of obj.data.Posts) {
-    const postCombine = document.createElement("div")
-    postCombine.className="post-combine"
-    postCombine.innerHTML=`
+   if  ( post.comments === null)   post.comments =  []
+    const postCombine = document.createElement("div");
+    postCombine.className = "post-combine";
+    postCombine.innerHTML = `
     <div class="post-card" id="post-${post.id}">
       <div class="first-part">
         <div class="post-header">
@@ -34,12 +37,11 @@ export async function home() {
             }.png?size=50x50" class="avatar" />
             <span class="username">${post.username}</span>
           </div>
-          <span class="post-time">${post.timeFormatted}</span>
+          <span class="post-time">${timeFormat(post.time)}</span>
         </div>
 
         <h2 class="post-title">${post.title}</h2>
 
-        ${post.imagePath ? `<img src="${post.imagePath}" class="image" />` : ""}
 
         <p class="post-description">${post.description}</p>
         <div class="post-tags">
@@ -83,25 +85,83 @@ export async function home() {
                 }
               </style>
             </div>
+
             <input type="hidden" name="postID" value="${post.id}" />
           </form>
         </div>
       </div>
+
       <div class="second-part" id="post-${post.id}">
-        <!-- comments and comment form will go here -->
+
+             <div class="comment">
+            <form action="/comment" method="post">
+              <input type="hidden" name="postID" value="{{.Id}}" />
+              <img src="https://robohash.org/{{$.UserActive}}.png?size=50x50" />
+              <input type="text" name="comment" placeholder="Add Comment" required /><br />
+              <button type="submit">Add</button>
+            </form>
+          </div>
+
+
+             <div class="commentaires">
+      <h2 class="comment-title">Comments</h2>
+      
+      ${post.comments.map(
+        (comment) => `
+        <div class="comments">
+          <img src="https://robohash.org/${comment.Username}.png?size=50x50" />
+          <div class="comment-content">
+            <p class="user"><strong>${comment.Username}</strong></p>
+            <p class="comm">${comment.Comment}</p>
+            <div class="comment-actions">
+              <span class="time">${timeFormat(comment.time)}</span>
+              <div class="comment-reactions">
+                <div class="reactionComment">
+                  <span class="span-like ${
+                    comment.UserReactionComment === 1 ? "active-like" : ""
+                  }">
+                    ${comment.TotalLikes}
+                  </span>
+                  <button class="comment-like-btn ${
+                    comment.UserReactionComment === 1 ? "active-like" : ""
+                  }" data-id="${comment.Id}" data-reaction="1" type="button">
+                    <i class="fa-solid fa-thumbs-up"></i>
+                  </button>
+                </div>
+                <div class="reactionComment">
+                  <span class="span-dislike ${
+                    comment.UserReactionComment === -1 ? "active-dislike" : ""
+                  }">
+                    ${comment.TotalDislikes}
+                  </span>
+                  <button class="comment-dislike-btn ${
+                    comment.UserReactionComment === -1 ? "active-dislike" : ""
+                  }" data-id="${comment.Id}" data-reaction="-1" type="button">
+                    <i class="fa-solid fa-thumbs-down"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      `
+      ).join("")}
+    </div>
+
+          
       </div>
     </div>
 `;
-allPost.appendChild(postCombine)
+    allPost.appendChild(postCombine);
   }
   // end post container
   container.innerHTML = "";
   header.innerHTML = Header;
   Postform.innerHTML = PostForm;
-  
-  container.append(allPost)
+
   container.appendChild(header);
   container.appendChild(Postform);
+  container.append(allPost);
   const logoutButton = header.querySelector(".logout");
   let createButton = header.querySelector(".create");
   logoutButton.addEventListener("click", Logout);
