9b858c549fee5c70e2c70008ba7078f6d50e9c1a
AhmedBaid <baidahmed2005@gmail.com>
2025-08-27 16:33:43 +0100

    merge the work of reda websoket connection-messages

--- backend/database/query.sql
+++ backend/database/query.sql
@@ -72,7 +72,7 @@ CREATE TABLE   IF NOT EXISTS messages (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     sender_id INT NOT NULL,
     receiver_id INT NOT NULL,
-is_read BOOLEAN DEFAULT FALSE,    message TEXT NOT NULL,
+    is_read BOOLEAN DEFAULT FALSE,    message TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (sender_id) REFERENCES users(id),
     FOREIGN KEY (receiver_id) REFERENCES users(id)
